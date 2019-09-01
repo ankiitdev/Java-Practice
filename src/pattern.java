37ec6938e265d7977e82c2a37198b533ece5da09
ankiitdev <ankiitdev@gmail.com>
2019-09-02 00:15:34 +0530

    Second commit

--- src/pattern.java
+++ src/pattern.java
@@ -22,14 +22,7 @@ public class pattern {
         }
         System.out.print("\n");
 
-        for(int i=1; i<=5; i++)
-        {
-            for(int j=5; j<i; j--)
-            {
-                System.out.print(" ");
-            }
-            System.out.print("*");
-        }
+
 
 
     }
